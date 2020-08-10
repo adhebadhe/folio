ea3c290dcf6e78b461d0a09f9f2c8e63756e5ce6
adhebadhe <bendpratte@gmail.com>
2020-08-10 13:49:31 -0500

    first commit

---
+++ src/views/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="Header-img">
+      <div className="title">Ben Pratte</div>
+    </div>
+  )
+}
+
+export default Header;
